2cb84d490a2e89a4b9e048b929da8ef049570890
Walkways <48722285+Walkways@users.noreply.github.com>
2024-01-12 19:11:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials =  file("gcp-credentials.json")
+  #credentials =  file("gcp-credentials.json")
   project     = "zinc-strategy-393412"
   region      = "us-central1"
 }
